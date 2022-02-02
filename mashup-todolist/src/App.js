e7080dbae96f804cfb5ca079762809ba0b33e558
jina jeon <takethisbus@gmail.com>
2022-02-02 19:04:22 +0900

    set all components & set events

--- mashup-todolist/src/App.js
+++ mashup-todolist/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import TodoTemplate from './components/TodoTemplate';
 import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
+import TodoCreate from './components/TodoCreate';
 
 const GlobalStyle = createGlobalStyle`
   body{
@@ -14,7 +16,10 @@ function App() {
     <>
       <GlobalStyle />
       <TodoTemplate>
-        <TodoHead/>
+        <TodoHead />
+        <TodoList>
+        </TodoList>
+        <TodoCreate/>
       </TodoTemplate>
     </>
   );
--- mashup-todolist/src/components/TodoTemplate.js
+++ mashup-todolist/src/components/TodoTemplate.js
@@ -18,7 +18,7 @@ const TodoTemplateBlock = styled.div`
     flex-direction: column;
 `;
 
-function TodoTemplate({ children}) {
+function TodoTemplate({ children }) {
     return (
         <TodoTemplateBlock>
             {children}
