86275d1cf13c2f115e324c43827a1030456ec5c0
jina jeon <takethisbus@gmail.com>
2022-02-02 21:01:28 +0900

    add function for create todos

--- mashup-todolist/src/components/TodoCreate.js
+++ mashup-todolist/src/components/TodoCreate.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styled, { css } from "styled-components";
 import { MdAdd } from 'react-icons/md';
+import { useTodoDispatch, useTodoNextId } from "../TodoContext";
 
 const CircleButton = styled.button`
     background: #38d9a9;
@@ -52,7 +53,7 @@ const InsertFormPositioner = styled.div`
     position: absolute;
 `;
 
-const InsertForm = styled.div`
+const InsertForm = styled.form`
     background: #f8f9fa;
     padding: 32px;
     padding-bottom: 72px;
@@ -73,14 +74,39 @@ const Input = styled.input`
 
 function TodoCreate() {
     const [open, setOpen] = useState(false);
+    const [value, setValue] = useState('');
+
     const onToggle = () => setOpen(!open);
+    const onChange = (e) => setValue(e.target.value);
+
+    const dispatch = useTodoDispatch();
+    const nextId = useTodoNextId();
+
+    const onSubmit = (e) => { 
+        e.preventDefault();
+        dispatch({
+            type: 'CREATE',
+            todo: {
+                id: nextId.current,
+                text: value,
+                done: false
+            }
+        });
+        setValue('');
+        setOpen(false);
+        nextId.current += 1;
+    };
 
     return (
         <>
             {open && (
                 <InsertFormPositioner>
-                    <InsertForm>
-                        <Input placeholder="할 일을 입력 후, Enter를 누르세요." autoFocus/>
+                    <InsertForm onSubmit={onSubmit}>
+                        <Input
+                            placeholder="할 일을 입력 후, Enter를 누르세요."
+                            autoFocus
+                            onChange={onChange}
+                            value={value}/>
                     </InsertForm>
                 </InsertFormPositioner>
             )}
