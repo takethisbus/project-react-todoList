d6453062c83b84870af5719601f9885685f571ae
jina jeon <takethisbus@gmail.com>
2022-02-02 19:52:11 +0900

    check state value

--- mashup-todolist/src/App.js
+++ mashup-todolist/src/App.js
@@ -4,6 +4,7 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoHead from './components/TodoHead';
 import TodoList from './components/TodoList';
 import TodoCreate from './components/TodoCreate';
+import { TodoProvider } from './TodoContext';
 
 const GlobalStyle = createGlobalStyle`
   body{
@@ -13,7 +14,7 @@ const GlobalStyle = createGlobalStyle`
 
 function App() {
   return (
-    <>
+    <TodoProvider>
       <GlobalStyle />
       <TodoTemplate>
         <TodoHead />
@@ -21,7 +22,7 @@ function App() {
         </TodoList>
         <TodoCreate/>
       </TodoTemplate>
-    </>
+    </TodoProvider>
   );
 }
 
